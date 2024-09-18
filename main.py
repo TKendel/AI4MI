560b01c6b00da3a2babb7a1e85f7d3f42247944d
InigoMora <47795034+InigoMora@users.noreply.github.com>
2024-09-18 16:58:47 +0200

    Changed the match statements to if statements
    For compatibility with python 3.9, because we could not have 3.11 running in Snellius, the match cases were adapted to if statements

--- main.py
+++ main.py
@@ -141,6 +141,25 @@ def runTraining(args):
 
     for e in range(args.epochs):
         for m in ['train', 'val']:
+            # Because we cannot get python 3.11 running in Snellius, we changed the match cases to if statements
+            if m == 'train':
+                net.train()
+                opt = optimizer
+                cm = Dcm
+                desc = f">> Training   ({e: 4d})"
+                loader = train_loader
+                log_loss = log_loss_tra
+                log_dice = log_dice_tra
+            if m == 'val':
+                net.eval()
+                opt = None
+                cm = torch.no_grad
+                desc = f">> Validation ({e: 4d})"
+                loader = val_loader
+                log_loss = log_loss_val
+                log_dice = log_dice_val
+            #If we ever get python 3.11, we can change to match and remove the upper two if statements
+            """
             match m:
                 case 'train':
                     net.train()
@@ -158,6 +177,7 @@ def runTraining(args):
                     loader = val_loader
                     log_loss = log_loss_val
                     log_dice = log_dice_val
+              """
 
             with cm():  # Either dummy context manager, or the torch.no_grad for validation
                 j = 0
