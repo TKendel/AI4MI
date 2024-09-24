1262efaeb5afbd8267ee0042694b947875bc47a5
TKendel <kendeltoni@gmail.com>
2024-09-24 21:06:32 +0200

    feat: updated converter to generic iterable

--- converter.py
+++ converter.py
@@ -1,23 +1,28 @@
-
-import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-from PIL import Image
-from torchvision import transforms
-
 import nibabel as nib
-img = cv.imread('data\segthor_train\\train\Patient_01\GT.nii\GT.nii')
 
-image = nib.load('data\segthor_train\\train\Patient_01\GT.nii\GT.nii')
+from os import walk, remove, rename
+from os.path import join, isfile
 
-# update data type:
-new_dtype = np.uint8  # for example to cast to int8.
-image.set_data_dtype(new_dtype)
+path = 'data\segthor_train\\train'
 
-nib.save(image, 'my_image_new_datatype.nii')
+# Iterate over files in directory
+for subdir, dirs, files in walk(path):
+    for file in files:
+        # Look for new files 
+        if file[-4:] == '.nii':
+            #TODO: Uncomment to rename the old file and make space for the new one
+            if isfile(join(subdir, 'GT.nii.gz')):
+                rename(join(subdir, 'GT.nii.gz'), join(subdir, 'GT_OG.nii.gz'))
 
-image = cv.imread('my_image_new_datatype.nii').dtype
+            image = nib.load(join(subdir, file))
 
+            # Update data type:
+            new_dtype = np.uint8
+            image.set_data_dtype(new_dtype)
 
+            # Compress to gzip
+            nib.save(image, join(subdir, 'GT.nii.gz'))
+
+            # Remove files
+            remove(join(subdir, file))
