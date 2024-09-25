06768cd9a6db69a2fe204472ba8868a355a4ee78
InigoMora <47795034+InigoMora@users.noreply.github.com>
2024-09-25 18:18:55 +0200

    Create data_to_nnunet.py, to set the data asa expected by the nnunet network
    found previously in nun-Net branch, just sharing in master branch

---
+++ data_to_nnunet.py
@@ -0,0 +1,29 @@
+#Format data for nnunet raw fodler
+
+import os
+import shutil
+
+
+
+patient_list = list(range(1,41)) #there are 40 patients
+
+for patient in patient_list:
+        volume_file = os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'Patient_'+f"{patient:02}"+'.nii.gz')
+        volume_destination = os.path.join('Dataset010_SEGT/nnUNet_raw/Dataset001_segthor/imagesTr/', 'SEGT_'+f"{patient:03}"+'_0000.nii.gz')
+        shutil.copyfile(volume_file, volume_destination)      #source --> destination
+
+        segmentation_file = os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'gt.nii.gz')
+        segmentation_destination = os.path.join('Dataset010_SEGT/nnUNet_raw/Dataset001_segthor/labelsTr/', 'SEGT_'+f"{patient:03}"+'.nii.gz')
+        shutil.copyfile(segmentation_file, segmentation_destination)      #source --> destination
+
+        print('Patient', patient, '/40')
+
+print("All files have been copied and formatted")
+
+
+
+
+
+
+
+
