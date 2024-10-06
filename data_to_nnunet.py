179ce5e41e23d8956a355bc974f3e409903fd9b1
InigoMora <inigoauzadelamora@bennington.edu>
2024-10-06 19:40:25 +0200

    Created new python env, imported Floor's affine transformations, modified data_to_nnunet.py to take an ID used for the task folder name

--- data_to_nnunet.py
+++ data_to_nnunet.py
@@ -4,16 +4,18 @@ import os
 import shutil
 
 
+id = 550        
+print(f"{id:03}")
 
 patient_list = list(range(1,41)) #there are 40 patients
 
 for patient in patient_list:
         volume_file = os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'Patient_'+f"{patient:02}"+'.nii.gz')
-        volume_destination = os.path.join('Dataset010_SEGT/nnUNet_raw/Dataset001_segthor/imagesTr/', 'SEGT_'+f"{patient:03}"+'_0000.nii.gz')
+        volume_destination = os.path.join(f'Dataset010_SEGT/nnUNet_raw_data_base/nnUNet_raw_data/Dataset{id:03}_segthor/imagesTr/', 'SEGT_'+f"{patient:03}"+'_0000.nii.gz')
         shutil.copyfile(volume_file, volume_destination)      #source --> destination
 
         segmentation_file = os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'gt.nii.gz')
-        segmentation_destination = os.path.join('Dataset010_SEGT/nnUNet_raw/Dataset001_segthor/labelsTr/', 'SEGT_'+f"{patient:03}"+'.nii.gz')
+        segmentation_destination = os.path.join(f'Dataset010_SEGT/nnUNet_raw_data_base/nnUNet_raw_data/Dataset{id:03}_segthor/labelsTr/', 'SEGT_'+f"{patient:03}"+'.nii.gz')
         shutil.copyfile(segmentation_file, segmentation_destination)      #source --> destination
 
         print('Patient', patient, '/40')
