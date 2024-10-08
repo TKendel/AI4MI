7bfeccaffa373457d1f19eba580d7d0d25923773
clairek.72 <claire.kuhlkin@gmail.com>
2024-10-08 11:05:42 +0200

    adjusted for 3d and greyscale images

--- UNet.py
+++ UNet.py
@@ -1,62 +1,56 @@
 import torch
 import torch.nn as nn
-from torchvision import models
 from torch.nn.functional import relu
 
-class UNet(nn.Module):
+class UNet3D(nn.Module):
     def __init__(self, n_class, use_batchnorm=False, binary=False):
-        super(UNet, self).__init__()
+        super(UNet3D, self).__init__()
+        
         self.use_batchnorm = use_batchnorm
-        self.binary = binary
+        self.binary = binary  # If True, this will be binary segmentation (sigmoid), else multi-class (softmax)
         
         # Encoder
-        # In the encoder, convolutional layers with the Conv2d function are used to extract features from the input image. 
-        # Each block in the encoder consists of two convolutional layers followed by a max-pooling layer, with the exception of the last block which does not include a max-pooling layer.
-        # -------
-        # input: 572x572x3
-        self.e11 = nn.Conv2d(3, 64, kernel_size=3, padding=1) # output: 570x570x64
-        self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 568x568x64
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 284x284x64
+        self.e11 = nn.Conv3d(1, 64, kernel_size=3, padding=1)  # Input channels changed from 2D to 3D
+        self.e12 = nn.Conv3d(64, 64, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool3d(kernel_size=2, stride=2)
 
-        # input: 284x284x64
-        self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 282x282x128
-        self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 280x280x128
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 140x140x128
+        self.e21 = nn.Conv3d(64, 128, kernel_size=3, padding=1)
+        self.e22 = nn.Conv3d(128, 128, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool3d(kernel_size=2, stride=2)
 
-        # input: 140x140x128
-        self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 138x138x256
-        self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 136x136x256
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 68x68x256
+        self.e31 = nn.Conv3d(128, 256, kernel_size=3, padding=1)
+        self.e32 = nn.Conv3d(256, 256, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool3d(kernel_size=2, stride=2)
 
-        # input: 68x68x256
-        self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1) # output: 66x66x512
-        self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1) # output: 64x64x512
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 32x32x512
+        self.e41 = nn.Conv3d(256, 512, kernel_size=3, padding=1)
+        self.e42 = nn.Conv3d(512, 512, kernel_size=3, padding=1)
+        self.pool4 = nn.MaxPool3d(kernel_size=2, stride=2)
 
-        # input: 32x32x512
-        self.e51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1) # output: 30x30x1024
-        self.e52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1) # output: 28x28x1024
-
+        self.e51 = nn.Conv3d(512, 1024, kernel_size=3, padding=1)
+        self.e52 = nn.Conv3d(1024, 1024, kernel_size=3, padding=1)
 
         # Decoder
-        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.d11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-        self.d12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.upconv1 = nn.ConvTranspose3d(1024, 512, kernel_size=2, stride=2)
+        self.d11 = nn.Conv3d(1024, 512, kernel_size=3, padding=1)
+        self.d12 = nn.Conv3d(512, 512, kernel_size=3, padding=1)
 
-        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.d21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.d22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.upconv2 = nn.ConvTranspose3d(512, 256, kernel_size=2, stride=2)
+        self.d21 = nn.Conv3d(512, 256, kernel_size=3, padding=1)
+        self.d22 = nn.Conv3d(256, 256, kernel_size=3, padding=1)
 
-        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.d31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.d32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.upconv3 = nn.ConvTranspose3d(256, 128, kernel_size=2, stride=2)
+        self.d31 = nn.Conv3d(256, 128, kernel_size=3, padding=1)
+        self.d32 = nn.Conv3d(128, 128, kernel_size=3, padding=1)
 
-        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.d41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.d42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.upconv4 = nn.ConvTranspose3d(128, 64, kernel_size=2, stride=2)
+        self.d41 = nn.Conv3d(128, 64, kernel_size=3, padding=1)
+        self.d42 = nn.Conv3d(64, 64, kernel_size=3, padding=1)
 
         # Output layer
-        self.outconv = nn.Conv2d(64, n_class, kernel_size=1)
+        self.outconv = nn.Conv3d(64, n_class, kernel_size=1)
+
+        # Initialize weights
+        self.init_weights()
 
     def forward(self, x):
         # Encoder
@@ -78,7 +72,7 @@ class UNet(nn.Module):
 
         xe51 = relu(self.e51(xp4))
         xe52 = relu(self.e52(xe51))
-        
+
         # Decoder
         xu1 = self.upconv1(xe52)
         xu11 = torch.cat([xu1, xe42], dim=1)
@@ -103,13 +97,18 @@ class UNet(nn.Module):
         # Output layer
         out = self.outconv(xd42)
 
+        # Apply final activation
+        if self.binary:
+            out = torch.sigmoid(out)  # Binary segmentation
+        else:
+            out = torch.softmax(out, dim=1)  # Multi-class segmentation
+
         return out
 
     def init_weights(self):
-        # Weight initialization using He initialization
         for m in self.modules():
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            if isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose3d):
                 nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, nn.BatchNorm3d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
