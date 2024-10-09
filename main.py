bad2b8ae9abf1e6fe5bb28d8a2f6e5fcc1a866cf
mirthekemp <mirthe.kemp@gmail.com>
2024-10-09 17:45:22 +0200

    added floors code with metrics and other losses to the main file

--- main.py
+++ main.py
@@ -48,14 +48,11 @@ from utils import (Dcm,
                    probs2class,
                    tqdm_,
                    dice_coef,
+                   iou_coef,
                    save_images)
-from metrics import volume_dice
+from metrics import volume_dice, volume_iou, volume_hausdorff, slice_hausdorff
 
-from losses import CrossEntropy, DiceLoss
-
-from losses import (CrossEntropy)
-from losses import (BinaryFocalLoss) # added
-
+from losses import CrossEntropy, DiceLoss, BinaryFocalLoss
 
 datasets_params: dict[str, dict[str, Any]] = {}
 # K for the number of classes
@@ -77,10 +74,10 @@ def setup(args) -> tuple[nn.Module, Any, Any, DataLoader, DataLoader, int]:
     net.init_weights()
     net.to(device)
 
-   #learning rate & adam optimizer
+    #learning rate & adam optimizer
     lr = 0.0005
-    #optimizer = torch.optim.Adam(net.parameters(), lr=lr, betas=(0.9, 0.999))
-    optimizer = torch.optim.AdamW(net.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0.01)
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr, betas=(0.9, 0.999))
+    # optimizer = torch.optim.AdamW(net.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0.01)
 
     #adding a learning rate scheduler
     #scheduler = lr_scheduler.PolynomialLR(optimizer, total_iters=5, power=1.0)
@@ -139,40 +136,42 @@ def setup(args) -> tuple[nn.Module, Any, Any, DataLoader, DataLoader, int]:
 
 def runTraining(args):
     if args.dataset =='SEGTHOR': 
-        sampleT = 30
         sampleV = 10
     elif args.dataset =='TOY':
-        sampleT = 1000
         sampleV = 100
     
     print(f">>> Setting up to train on {args.dataset} with {args.mode}")
     net, optimizer, device, train_loader, val_loader, K = setup(args)
 
     if args.mode == "full":
-        # loss_fn = CrossEntropy(idk=list(range(K)))  # Supervise both background and foreground
-        # Changed to BinaryFocalLoss
-        fl_loss_fn = BinaryFocalLoss(idk=list(range(K))) 
         loss_fn = CrossEntropy(idk=list(range(K)))  # Supervise both background and foreground
         dloss_fn = DiceLoss(idk=list(range(K)))  # Supervise both background and foreground
+        fl_loss_fn = BinaryFocalLoss(idk=list(range(K))) # Supervise both background and foreground
     elif args.mode in ["partial"] and args.dataset in ['SEGTHOR', 'SEGTHOR_STUDENTS']:
-        # loss_fn = CrossEntropy(idk=[0, 1, 3, 4])  # Do not supervise the heart (class 2)
-        # Changed to BinaryFocalLoss
-        fl_loss_fn = BinaryFocalLoss(idk=[0, 1, 3, 4])
+        loss_fn = CrossEntropy(idk=[0, 1, 3, 4])  # Do not supervise the heart (class 2)
+        dloss_fn = DiceLoss(idk=[0, 1, 3, 4])  # Do not supervise the heart (class 2)
+        fl_loss_fn = BinaryFocalLoss(idk=[0, 1, 3, 4]) # Do not supervise the heart (class 2)
     else:
         raise ValueError(args.mode, args.dataset)
 
     # Notice one has the length of the _loader_, and the other one of the _dataset_
     log_loss_tra: Tensor = torch.zeros((args.epochs, len(train_loader)))
-    log_dice_tra: Tensor = torch.zeros((args.epochs, len(train_loader.dataset), K))
-    # Added log_focal_tra
-    log_focal_tra: Tensor = torch.zeros((args.epochs, len(train_loader)))
     log_dloss_tra: Tensor = torch.zeros((args.epochs, len(train_loader)))  # To store the loss for each batch in every epoch during training. lwn(train_laoder) = nr of batches
+    log_focal_tra: Tensor = torch.zeros((args.epochs, len(train_loader)))
+    log_dice_tra: Tensor = torch.zeros((args.epochs, len(train_loader.dataset), K)) # To store the Dice coefficient for each sample (image) in the training set for each class in every epoch --> nr of training samples
+    log_IOU_tra: Tensor = torch.zeros((args.epochs, len(train_loader.dataset), K)) 
 
     log_loss_val: Tensor = torch.zeros((args.epochs, len(val_loader)))
     log_dloss_val: Tensor = torch.zeros((args.epochs, len(train_loader)))  # To store the loss for each batch in every epoch during training. lwn(train_laoder) = nr of batches
-    log_dice_val: Tensor = torch.zeros((args.epochs, len(val_loader.dataset), K))
     log_focal_val: Tensor = torch.zeros((args.epochs, len(val_loader)))
+    log_dice_val: Tensor = torch.zeros((args.epochs, len(val_loader.dataset), K))
+    log_IOU_val: Tensor = torch.zeros((args.epochs, len(val_loader.dataset), K))
     log_3d_dice_val = torch.zeros((args.epochs, sampleV, K))  # Shape: (epochs, num_patients, K)
+    log_3d_IOU_val = torch.zeros((args.epochs, sampleV, K))  # Shape: (epochs, num_patients, K)
+    log_hausdorff_val = torch.zeros((args.epochs, sampleV, K-1))  # Shape: (epochs, num_patients, K)
+    #log_95hausdorff_val = torch.zeros((args.epochs, sampleV, K-1))  # Shape: (epochs, num_patients, K)
+
+    log_slicehd_val = torch.zeros((args.epochs, sampleV, K-1)) 
 
     best_dice: float = 0
 
@@ -188,9 +187,9 @@ def runTraining(args):
                 loader = train_loader
                 log_loss = log_loss_tra
                 log_dloss = log_dloss_tra
-                log_dice = log_dice_tra
-                # Added for BinaryFocalLoss
                 log_focal = log_focal_tra
+                log_dice = log_dice_tra
+                log_IOU = log_IOU_tra
             if m == 'val':
                 net.eval()
                 opt = None
@@ -199,10 +198,14 @@ def runTraining(args):
                 loader = val_loader
                 log_loss = log_loss_val
                 log_dloss = log_dloss_val
-                log_dice = log_dice_val
-                # Added loss for BinaryFocalLoss
                 log_focal = log_focal_val
+                log_dice = log_dice_val                
                 log_3d_dice = log_3d_dice_val
+                log_3d_IOU = log_3d_IOU_val
+                log_hausdorff = log_hausdorff_val
+                #log_95hausdorff = log_95hausdorff_val
+                log_slicehd = log_slicehd_val
+
                 all_predictions = [] # store the predictions each epoch
                 all_gt_slices = [] #store the gts each epoch
             #If we ever get python 3.11, we can change to match and remove the upper two if statements
@@ -253,21 +256,25 @@ def runTraining(args):
                         all_gt_slices.append(gt.cpu())
                         
                     log_dice[e, j:j + B, :] = dice_coef(gt, pred_seg)  # One DSC value per sample and per class
+                    log_IOU[e, j:j + B, :] = iou_coef(gt, pred_seg)  # One iou value per sample and per class
                         # e: The current epoch.
                         # j:j + B: This slices the tensor for the current batch, where:
                         # j is the start index for the current batch in the log_dice array.
                         # j + B is the end index for this batch (B is the batch size, typically 8 in this case).
                         # --> log_dice.shape = (num_epochs, num_samples, num_classes)
 
+                    # cross entropy loss 
+                    loss = loss_fn(pred_probs, gt)
+                    log_loss[e, i] = loss.item()  # One loss value per batch (averaged in the loss)
+
+                    # dice loss
+                    dloss = dloss_fn(pred_probs, gt)
+                    log_dloss[e, i] = dloss.item() 
+
                     # Compute focal loss
                     floss = fl_loss_fn(pred_probs, gt)
                     log_focal[e, i] = floss.item()  # One loss value per batch (averaged in the loss)
 
-                    # todo focal: compute focal loss
-                    # floss = binary_focal_loss(pred_probs, gt)
-                    # log_loss[e, i] = floss.item()  # One loss value per batch (averaged in the loss)
-                    dloss = dloss_fn(pred_probs, gt)
-                    log_dloss[e, i] = dloss.item() 
 
                     if opt:  # Only for training
                         floss.backward() #todo focal: change to floss
@@ -286,43 +293,79 @@ def runTraining(args):
                     j += B  # Keep in mind that _in theory_, each batch might have a different size
                     # For the DSC average: do not take the background class (0) into account:
                     postfix_dict: dict[str, str] = {"Dice": f"{log_dice[e, :j, 1:].mean():05.3f}",
+                                                    "IoU": f"{log_IOU[e, :j, 1:].mean():05.3f}",
                                                     "Loss": f"{log_loss[e, :i + 1].mean():5.2e}",
-                                                    "Focal Loss": f"{log_focal[e, :i + 1].mean():05.2e}", # Adding the focal loss
+                                                    "Focal Loss": f"{log_focal[e, :i + 1].mean():05.3f}",
                                                     "dLoss": f"{log_dloss[e, :i + 1].mean():5.2e}"}
+                    
                     if K > 2:
-                        postfix_dict |= {f"Dice-{k}": f"{log_dice[e, :j, k].mean():05.3f}"
-                                         for k in range(1, K)}
+                        postfix_dict |= {f"Dice-{k}": f"{log_dice[e, :j, k].mean():05.3f}" for k in range(1, K)}
+                        postfix_dict |= {f"IoU-{k}": f"{log_IOU[e, :j, k].mean():05.3f}" for k in range(1, K)}
                     tq_iter.set_postfix(postfix_dict)
 
             if m == 'val':
                 all_predictions_tensor = torch.cat(all_predictions, dim=0)
                 all_gt_tensor = torch.cat(all_gt_slices, dim=0) 
                 path_to_slices = os.path.join("data", "SEGTHOR", "val", "img")
-                dice_scores_per_patient = volume_dice(all_predictions_tensor, all_gt_tensor, path_to_slices)
-                print(dice_scores_per_patient)
-                for patient_idx, (patient, dice_scores) in enumerate(dice_scores_per_patient.items()):
-                    rounded_dice_scores = [float(f"{score:05.3f}") for score in dice_scores]
-                    rounded_dice_tensor = torch.tensor(rounded_dice_scores, dtype=torch.float32)
-                    log_3d_dice[e, patient_idx, :] = rounded_dice_tensor
 
-                print(f"3d Dice Score (averaged over all patients and classes): {log_3d_dice[e, :, 1:].mean():05.3f}")
-                if K > 2:
-                    for k in range(1, K):
-                        print(f"3dDice-{k}: {log_3d_dice[e, :, k].mean():05.3f}")
+                # calculating the 3d sccores
+                dice_scores_per_patient = volume_dice(all_predictions_tensor, all_gt_tensor, path_to_slices)
+                iou_scores_per_patient = volume_iou(all_predictions_tensor, all_gt_tensor, path_to_slices)
+                hausdorff_per_patient = volume_hausdorff(all_predictions_tensor, all_gt_tensor, path_to_slices, K, hd_95=False)
+                #_95hausdorf_per_patient = volume_hausdorff(all_predictions_tensor, all_gt_tensor, path_to_slices, K, hd_95=True)
+                # print(dice_scores_per_patient)
+                slice_based_hd_per_patient = slice_hausdorff(all_predictions_tensor, all_gt_tensor, path_to_slices,K)
+
+                for patient_idx, (patient, dice_scores) in enumerate(dice_scores_per_patient.items()):
+                    log_3d_dice[e, patient_idx, :] = dice_scores.to(dtype=log_3d_dice.dtype, device=log_3d_dice.device)
+
+                for patient_idx, (patient, iou_score) in enumerate(iou_scores_per_patient.items()):
+                    log_3d_IOU[e, patient_idx, :] = iou_score.to(dtype=log_3d_IOU.dtype, device=log_3d_IOU.device)
+
+                for patient_idx, (patient, hausdorff) in enumerate(hausdorff_per_patient.items()):
+                    log_hausdorff[e, patient_idx, :] = hausdorff.to(dtype=log_hausdorff.dtype, device=log_hausdorff.device)
+            
+
+                                # for patient_idx, (patient, _95hausdorff) in enumerate(_95hausdorf_per_patient.items()):
+                #     log_95hausdorff[e, patient_idx, :] = _95hausdorff.to(dtype=log_95hausdorff.dtype, device=log_95hausdorff.device)
+            
+                for patient_idx, (patient, sb_hd) in enumerate(slice_based_hd_per_patient.items()):
+                    log_slicehd[e, patient_idx, :] = sb_hd.to(dtype=log_slicehd.dtype, device=log_slicehd.device)
+
+                for metric_name, log_metric in [("3dDice", log_3d_dice), ("3dIOU", log_3d_IOU)]:  #
+                    print(f"{metric_name}: {log_metric[e, :, 1:].mean():05.3f}\t", end='')  #exclude background from mean
+                    if K > 2:
+                        for k in range(1, K):
+                            print(f"{metric_name}-{k}: {log_metric[e, :, k].mean():05.3f}\t", end='')   #print haussdorf for all organs 
+                    print()
+                
+                for metric_name, log_metric in [("HD", log_hausdorff), ("slHD", log_slicehd)]: #("95HD", log_95hausdorff), 
+                    print(f"{metric_name}: {log_metric[e, :, :].mean():05.3f}\t", end='')  
+                    if K > 2:
+                        for k in range(0, 4):
+                            print(f"{metric_name}-{k+1}: {log_metric[e, :, k].mean():05.3f}\t", end='')   #print haussdorf for all organs (we exluded the background so therefore k+1 to keep the labelling correct)
+                    print()
+
 
         
-        print(log_3d_dice[e, :i + 1].mean())
         # I save it at each epochs, in case the code crashes or I decide to stop it early
         np.save(args.dest / "loss_tra.npy", log_loss_tra)
         np.save(args.dest / "dloss_tra.npy", log_dloss_tra)
         np.save(args.dest / "dice_tra.npy", log_dice_tra)
-        np.save(args.dest / "focal_tra.npy", log_focal_tra)
+        np.save(args.dest / "iou_tra.npy", log_IOU_tra)
+        np.save(args.dest / "floss_tra.npy", log_focal_tra)
+        
         np.save(args.dest / "loss_val.npy", log_loss_val)
         np.save(args.dest / "dloss_val.npy", log_dloss_val)
-        np.save(args.dest / "dice_val.npy", log_dice_val)
         np.save(args.dest / "focal_val.npy", log_focal_val)
+        np.save(args.dest / "dice_val.npy", log_dice_val)
+        np.save(args.dest / "iou_val.npy", log_IOU_val)
 
         np.save(args.dest / "3ddice_val.npy", log_3d_dice_val)
+        np.save(args.dest / "3dIOU_val.npy", log_3d_IOU_val)
+        np.save(args.dest / "slHD.npy", log_slicehd)
+        np.save(args.dest / "HD.npy", log_hausdorff)
+        #np.save(args.dest / "95HD.npy", log_95hausdorff)
         
 
         current_dice: float = log_dice_val[e, :, 1:].mean().item()
