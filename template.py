8476faa8c0c3738fdc6db0df735a984e9cce63fb
TKendel <kendeltoni@gmail.com>
2024-10-10 10:05:42 +0200

    feat: added logging to template

--- template.py
+++ template.py
@@ -9,9 +9,12 @@ from preprocessing import Preprocessing
 tableXY = np.array([False])
 current_patient = '03'
 
-for filepath in glob.iglob('data\SEGTHOR\\train\img\*.png'):
-    if filepath[-11:-9] == current_patient:
-        ct_scan = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
+print('Processing images')
+
+try:
+    for filepath in glob.iglob('data/SEGTHOR/train/img/*.png'):
+        if filepath[-11:-9] == current_patient:
+            ct_scan = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
 
         pp = Preprocessing(ct_scan, f'data\SEGTHOR_tmp\\train\img\{filepath[-19:]}')
         pp.bilateralFilter()
@@ -31,5 +34,12 @@ for filepath in glob.iglob('data\SEGTHOR\\train\img\*.png'):
         # pp.gammaCorrection(2)
         pp.save()
 
-    current_patient = filepath[-11:-9]
+        current_patient = filepath[-11:-9]
+
+    print('Done with the processing of the images')
+
+except:
+    print("An error occured while loading the preprocessing pipeline")
+
+
 
