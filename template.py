2a7339e8f474e90bd8815d99920e2dfa4e52bc54
TKendel <37151944+TKendel@users.noreply.github.com>
2024-10-14 13:38:57 +0200

    Revert "Feat/pre processing"

--- template.py
+++ template.py
@@ -9,39 +9,27 @@ from preprocessing import Preprocessing
 tableXY = np.array([False])
 current_patient = '03'
 
-print('Processing images')
+for filepath in glob.iglob('data\SEGTHOR\\train\img\*.png'):
+    if filepath[-11:-9] == current_patient:
+        ct_scan = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
 
-try:
-    for filepath in glob.iglob('data/SEGTHOR/train/img/*.png'):
-        if filepath[-11:-9] == current_patient:
-            ct_scan = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
+        pp = Preprocessing(ct_scan, f'data\SEGTHOR_tmp\\train\img\{filepath[-19:]}')
+        pp.bilateralFilter()
+        tableXY = pp.removeTable(tableXY)
+        pp.equalize()
+        # pp.gammaCorrection(2)
+        pp.save()
+        
+    else:
+        tableXY = np.array([False])
+        ct_scan = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
 
-            pp = Preprocessing(ct_scan, f'data/SEGTHOR_tmp/train/img/{filepath[-19:]}')
+        pp = Preprocessing(ct_scan, f'data\SEGTHOR_tmp\\train\img\{filepath[-19:]}')
+        pp.bilateralFilter()
+        tableXY = pp.removeTable(tableXY)
+        pp.equalize()
+        # pp.gammaCorrection(2)
+        pp.save()
 
-            pp.bilateralFilter()
-            tableXY = pp.removeTable(tableXY)
-            pp.equalize()
-            # pp.gammaCorrection(2)
-            pp.save()
-            
-        else:
-            tableXY = np.array([False])
-            ct_scan = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
-
-            pp = Preprocessing(ct_scan, f'data/SEGTHOR_tmp/train/img/{filepath[-19:]}')
-
-            pp.bilateralFilter()
-            tableXY = pp.removeTable(tableXY)
-            pp.equalize()
-            # pp.gammaCorrection(2)
-            pp.save()
-
-        current_patient = filepath[-11:-9]
-
-    print('Done with the processing of the images')
-
-except:
-    print("An error occured while loading the preprocessing pipeline")
-
-
+    current_patient = filepath[-11:-9]
 
