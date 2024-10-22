b24230a1b76855b0a963f471654894793fb07af7
InigoMora <47795034+InigoMora@users.noreply.github.com>
2024-10-22 12:00:26 +0200

    cleaned and further commented our own transfomration implementation

--- affine_transform.py
+++ affine_transform.py
@@ -1,22 +1,26 @@
 #Afffine transform on segmentations
+# This is our own implementation of the aligment of the heart volumes
 
-#import cv2
+# This file reads both GT of patient 27, and utilizes the correct gt to allign the other, calculating
+# the distance to then apply it to the rest of the gt of all other patients.
+ 
+
+        # IMPORTS
 import numpy as np
 import nibabel as nib
 import os
 
 from scipy.ndimage import center_of_mass
-#from scipy.spatial.transform.Rotation import align_vectors
-#from scipy.ndimage import rotate
 from scipy.spatial.transform import Rotation as R
 from scipy.ndimage import rotate
 
 
-
+        # LOAD NII.GZ GT VOLUMES
 # Reads and saves the same nii file
 reference = nib.load('data/segthor_train/train/Patient_27/GT2.nii.gz')  #loaad reference (correct) .nii.gz
 ref_np = np.array(reference.dataobj)    # to np arraay
 
+        #EXAMPLE ON HOW TO SAVE THE VOLUME TO FILE
 #output = nib.Nifti1Image(ref_np, reference.affine)
 #https://stackoverflow.com/questions/28330785/creating-a-nifti-file-from-a-numpy-array
 #np.eye
@@ -24,11 +28,10 @@ ref_np = np.array(reference.dataobj)    # to np arraay
 #Save image
 #nib.save(output,  os.path.join('data/segthor_train/train/Patient_27', 'test.nii.gz') )
 
-
 #Check for unique labels, print if needed
 #uniq = np.unique(ref_np)
 
-
+        #DIVIDES THE LAYERS OF EACH MASK - THE OTHER LAYERS AND SAVING HAVE BEEN COMMENTED OUT, BUT LEFT FOR REFERENCE
 #Save individual layers
 #single_layer = np.where(ref_np == 1, ref_np, 0) #np.where(conndition, if yes, if not) # ESOPHAGUS - MASK 1
 #output = nib.Nifti1Image(single_layer, reference.affine)
@@ -47,15 +50,14 @@ heart_ref = np.where(ref_np == 2, ref_np, 0) #np.where(conndition, if yes, if no
 #nib.save(output,  os.path.join('data/segthor_train/train/Patient_27', 'mask4.nii.gz') )
 
 
-
-
+        #LOADS SHIFTED HEART
 broken = nib.load('data/segthor_train/train/Patient_27/GT.nii.gz')  #load image to fix .nii.gz
 broken_np = np.array(broken.dataobj)    # to np arraay
 broken_heart = np.where(broken_np == 2, broken_np, 0) # HEART - MASK 2
 
 
 
-
+        #ALIGNMENT PROCESS
 #First rotate, as it is done at the center of the volume, not heart
 broken_heart = rotate(broken_heart, angle=-26, reshape=False)   #Rotate matrix #reshape=False makes sure not to expand matrix (crop when rotating)
 #https://stackoverflow.com/questions/53171057/numpy-matrix-rotation-for-any-degrees
@@ -72,6 +74,45 @@ print('Distance:', x,y,z)
 
 
 
+
+#AFTER CALCULATING TRANSFORMATION, APPLY SHIFTING TO ALL PATIENTS
+patient_list = list(range(1,41))        #there are 40 patients
+
+#f"{patient_list[0]:02} #some f string and regex refreshing, used when creating the file path
+#https://stackoverflow.com/questions/3505831/in-python-how-do-i-convert-a-single-digit-number-into-a-double-digits-string
+
+for patient in patient_list:
+        broken = nib.load( os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'GT.nii.gz') )  #loaad image to fix .nii.gz
+        broken_np = np.array(broken.dataobj)    # to np arraay
+        broken_heart = np.where(broken_np == 2, broken_np, 0) # HEART - MASK 2
+        broken_but_heart = np.where(broken_np != 2, broken_np, 0) # Errase the heart
+
+        broken_heart = rotate(broken_heart, angle=-26, reshape=False)   #Rotate matrix
+        #We don't want partial classes (between 0 and 2), so we round everything that is not 0 to 2
+        broken_heart =  np.where(broken_heart > 0, 2, 0)#.astype(np.int8)  #because we are reseting to 2, we get an error for sending int64, use .astype(np.int8) [used later on]
+
+        #Transform broken heart
+        broken_heart = np.roll(broken_heart, round(x), axis=0)
+        broken_heart = np.roll(broken_heart, round(y), axis=1)
+        broken_heart = np.roll(broken_heart, round(z), axis=2)
+
+        reconstruction = broken_but_heart
+        reconstruction = np.where(broken_heart != 2, reconstruction, 2).astype(np.uint8)    #where broken_heart does not have a 2, keep it as is, else change to a 2
+
+        output = nib.Nifti1Image(reconstruction, broken.affine)
+        nib.save(output,  os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'gt.nii.gz') )
+        print('Patient', patient, '/40')
+
+print("All exports have been saved")
+
+
+
+
+
+
+
+#FOR REFERENCE, BELOW YOU MAY FIND THE PROCESS DESCRIBED ABOVE APPLIED TO A SINGLE SHIFTED VOLUME
+
 #Transform broken heart
 """
 broken_heart = np.roll(broken_heart, round(x), axis=0)
@@ -86,7 +127,6 @@ broken_heart = np.roll(broken_heart, round(z), axis=2)
 output = nib.Nifti1Image(broken_heart, reference.affine)
 nib.save(output,  os.path.join('data/segthor_train/train/Patient_27', 'broken_heart.nii.gz') )
 """
-
 
 #Example on patient - heart shift and rotation - full reconstruction export
 """
@@ -118,35 +158,3 @@ output = nib.Nifti1Image(reconstruction, broken.affine)
 nib.save(output,  os.path.join('data/segthor_train/train/Patient_17', 'broken_heart.nii.gz') )
 """
 
-
-
-
-
-patient_list = list(range(1,41))        #there are 40 patients
-
-#f"{patient_list[0]:02}
-#https://stackoverflow.com/questions/3505831/in-python-how-do-i-convert-a-single-digit-number-into-a-double-digits-string
-
-for patient in patient_list:
-        broken = nib.load( os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'GT.nii.gz') )  #loaad image to fix .nii.gz
-        broken_np = np.array(broken.dataobj)    # to np arraay
-        broken_heart = np.where(broken_np == 2, broken_np, 0) # HEART - MASK 2
-        broken_but_heart = np.where(broken_np != 2, broken_np, 0) # Errase the heart
-
-        broken_heart = rotate(broken_heart, angle=-26, reshape=False)   #Rotate matrix
-        #We don't want partial classes (between 0 and 2), so we round everything that is not 0 to 2
-        broken_heart =  np.where(broken_heart > 0, 2, 0)#.astype(np.int8)  #because we are reseting to 2, we get an error for sending int64, use .astype(np.int8) [used later on]
-
-        #Transform broken heart
-        broken_heart = np.roll(broken_heart, round(x), axis=0)
-        broken_heart = np.roll(broken_heart, round(y), axis=1)
-        broken_heart = np.roll(broken_heart, round(z), axis=2)
-
-        reconstruction = broken_but_heart
-        reconstruction = np.where(broken_heart != 2, reconstruction, 2).astype(np.uint8)    #where broken_heart does not have a 2, keep it as is, else change to a 2
-
-        output = nib.Nifti1Image(reconstruction, broken.affine)
-        nib.save(output,  os.path.join('data/segthor_train/train/Patient_'+f"{patient:02}", 'gt.nii.gz') )
-        print('Patient', patient, '/40')
-
-print("All exports have been saved")
